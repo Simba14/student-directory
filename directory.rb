1cd89bc6b36c903e1216c84a1fac946761937fcf
Sean Blundell <seanblundell@gmail.com>
2017-03-06 13:22:12 +0700

    Save functionality added

--- directory.rb
+++ directory.rb
@@ -9,6 +9,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
 end
 
@@ -24,6 +25,8 @@ def process(selection)
       @students = input_students # input the students
     when "2"
       show_students # show the students
+    when "3"
+      save_students
     when "9"
       exit # programme will terminate
     else
@@ -106,6 +109,18 @@ def print_footer
     puts "Overall, we have #{@students.count} great students!".center(100)
   end
 
+end
+
+def save_students
+  # open the file for writing
+  file = File.open("students.csv", "w")
+  # iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
 end
 
 interactive_menu
